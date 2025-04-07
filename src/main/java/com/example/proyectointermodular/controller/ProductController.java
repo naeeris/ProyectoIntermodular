934d0e4c922a8b6102c8b943c312013160329116
naeeris <noeliacauqui25@gmail.com>
2025-04-07 21:53:22 +0200

    Creación de vistas y modificación dependencia en el pom para Spring Security.

--- src/main/java/com/example/proyectointermodular/controller/ProductController.java
+++ src/main/java/com/example/proyectointermodular/controller/ProductController.java
@@ -1,12 +1,18 @@
 package com.example.proyectointermodular.controller;
 
+import com.example.proyectointermodular.model.Product;
 import com.example.proyectointermodular.repository.MaterialRepository;
 import com.example.proyectointermodular.repository.ProductRepository;
 import com.example.proyectointermodular.repository.TypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
@@ -19,5 +25,129 @@ public class ProductController {
     private MaterialRepository materialRepository;
 
 
+    /**
+     * Metodo para redirigir al listado de productos.
+     * @return Devuelve una redirección a /products.
+     */
+    /*@GetMapping("")
+    public String index(){
+        return "redirect:/products";
+    }*/
+
+    /**
+     * Metodo para listar todos los productos
+     * @param model Producto.
+     * @return  Devuelve el listado de los productos en una vista HTML.
+     */
+    @GetMapping("")
+    public String findAll(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "product-list";
+    }
+
+    /**
+     * Metodo para mostrar los detalles de un solo producto.
+     * @param id ID del producto.
+     * @param model Producto.
+     * @return Devuelve el detalle de un producto en una vista HTML.
+     */
+    @GetMapping("/view/{id}")
+    public String findById(Model model, @PathVariable long id) {
+        model.addAttribute("product", productRepository.findById(id).get());
+        return "product-view";
+    }
+
+    /**
+     * Metodo para obtener un formulario vacio.
+     * @param model Producto.
+     * @return Devuelve el formulario vacio de un producto en un vista HTML.
+     */
+    @GetMapping("/form")
+    public String getForm(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("types", typeRepository.findAll());
+        model.addAttribute("materials", materialRepository.findAll());
+        return "product-form";
+    }
+
+    /**
+     * Metodo para crear un nuevo producto
+     * @param model Producto.
+     * @return Devuelve uan redireccion a /products.
+     */
+    @PostMapping("")
+    public String createProduct(Model model){
+        productRepository.save(new Product());
+        return "redirect:/products";
+    }
+
+    /**
+     * Metodo para editar un producto a traves de un formulario completo.
+     * @param model Producto.
+     * @param id ID del producto.
+     * @return Devuelve el formulario vacio de un producto en una vista HTML.
+     * @return Devuelve una redireccion al formulario vacio para añadir un nuevo producto.
+     */
+    @GetMapping("/edit/{id}")
+    public String editForm(Model model, @PathVariable long id) {
+        //Verificamos si existe el ID en la base de datos
+        if(productRepository.existsById(id)){
+            productRepository.findById(id).ifPresent(product -> {
+                model.addAttribute("product", product);
+                model.addAttribute("types", typeRepository.findAll());
+                model.addAttribute("materials", materialRepository.findAll());
+            });
+            return "product-form";
+
+        //Si no encuentra el ID en la base de datos, redirige al formulario vacio para crear un nuevo producto
+        }else{
+            return "redirect:/products/form";
+        }
+    }
+
+    /**
+     * Metodo para actualizar la informacion de un producto en la base de datos.
+     * @param updatedProduct
+     * @param id
+     * @return
+     */
+    @PostMapping("/update/{id}")
+    public String updateProduct(@ModelAttribute Product updatedProduct, @PathVariable Long id){
+        //Si el ID esta en BD -> actualiza la siguiente info
+        if(productRepository.existsById(id)){
+            productRepository.findById(id).ifPresent(product -> {
+                //Actualizamos los atributos modificables
+                product.setName(updatedProduct.getName());
+                product.setType(updatedProduct.getType());
+                product.setMaterial(updatedProduct.getMaterial());
+                product.setDescription(updatedProduct.getDescription());
+                product.setPrice(updatedProduct.getPrice());
+
+                //Guardo la info actualizada en la BD
+                productRepository.save(product);
+            });
+
+            return "redirect:/products";
+
+        }else{
+
+            //Si no encuentra el ID, redirige al formulario vacio para crear un nuevo producto
+            return "redirect:/products/form";
+        }
+    }
+
+    /**
+     * Metodo para eliminar un producto teniendo en cuenta el ID.
+     * @param id ID del producto.
+     * @return
+     */
+    @GetMapping("/delete/{id}")
+    public String deleteById(@PathVariable Long id){
+        productRepository.findById(id).ifPresent(product -> {
+            productRepository.deleteById(product.getId());
+        });
+
+        return "redirect:/products";
+    }
 
 }
